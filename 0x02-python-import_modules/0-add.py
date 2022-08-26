ae63a99256a2d30c5a076a0e099cfbe19e98268a
black-jamesbond <kshedu197@gmail.com>
2022-08-26 13:19:51 -0700

    p

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
-def add(a,b):
-    """ My addition function
 
-    Agrs:
-        a: frist integer
-        b: second integer
+if __name__ =="__main__":
+    from add_0 import add
 
-    Returns:
-        the return value. a + b
+    a = 1
+    b = 2
 
-    """
-    return (a + b)
+    print ("{} + {} = {}".format(a,b,add(a,b)))
 
-print(add(1,2))
+
