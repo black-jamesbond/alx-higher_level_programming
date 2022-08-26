8f10f06a4185a347fe1f9e77e76e2e3699ac2e79
black-jamesbond <kshedu197@gmail.com>
2022-08-26 11:38:07 -0700

    p

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def add(a,b):
+    """ My addition function
+
+    Agrs:
+        a: frist integer
+        b: second integer
+
+    Returns:
+        the return value. a + b
+
+    """
+    return (a + b)
+
+print(add(1,2))
