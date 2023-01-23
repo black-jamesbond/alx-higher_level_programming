c3ef1e27d8eb0c2b03e053ec66524e39b282b1bc
black-jamesbond <kshedu197@gmail.com>
2023-01-23 16:05:47 +0100

    minor changes

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -13,7 +13,7 @@ def safe_print_list_integers(my_list=[], x=0):
     times = 0
     for i in range(x):
         try:
-            print ("{}".format(my_list[i]), end = "")
+            print ("{:d}".format(my_list[i]), end = "")
             times += 1
         except (ValueError,TypeError,IndexError):
             continue
