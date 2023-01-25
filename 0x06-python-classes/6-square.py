7ac458ea2dd267be7d981a91d360580991c75f23
black-jamesbond <kshedu197@gmail.com>
2023-01-25 22:36:42 +0100

    n

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -53,6 +53,8 @@ class Square:
             return
         
         [print("") for i in range(0, self.position[1])]
-        for i in range(self.__size):
-            print("_"*self.__position[0] + "#"*self.__size)
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
                 
