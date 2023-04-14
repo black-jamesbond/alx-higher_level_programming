ebfb2828423a22a60f432a5e1f623c04f7e0a44e
black-jamesbond <kshedu197@gmail.com>
2023-04-14 05:13:02 -0500

    @1

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -5,7 +5,7 @@ while (process.argv[i] !== undefined) {
 }
 if (i <= 2) {
   console.log('No argument');
-} else if (i === 3){
+} else if (i === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
